2bab085e46ae8f9cdf956f1cb2ee2647737e159c
Thamyris <thamyrissatana3@gmail.com>
2021-04-19 19:42:59 -0300

    js uppdated

---
+++ index.js
@@ -0,0 +1,33 @@
+const pt = document.querySelector(".ling");
+const nat = document.querySelector(".nat");
+const mat = document.querySelector(".mat");
+const hum = document.querySelector(".hum");
+const red = document.querySelector(".red");
+const resultBox = document.querySelector(".resultado");
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", somarMedia);
+
+function somarMedia(event) {
+  event.preventDefault();
+
+  const soma =
+    Number.parseInt(pt.value) +
+    Number.parseInt(nat.value) +
+    Number.parseInt(mat.value) +
+    Number.parseInt(hum.value) +
+    Number.parseInt(red.value);
+
+  const media = soma / 5;
+  console.log(media);
+
+  const resultDiv = document.createElement("div");
+  resultDiv.classList.add("result-div");
+
+  const resultSum = document.createElement("h2");
+  resultSum.classList.add("result-sum");
+  // resultSum.innerText = `${media}`;
+  resultBox.appendChild(resultSum);
+  resultSum.textContent = media;
+}
